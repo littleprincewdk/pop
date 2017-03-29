27a3126817a179332edfe23c488520e4139ba405
wudengke <2889110370@qq.com>
2017-03-29 11:34:56 +0800

    module

--- dist/xpop.js
+++ dist/xpop.js
@@ -1,7 +1,18 @@
 /**
  * Created by wudengke on 2017/3/12.
  */
-$(function(){
+(function (factory) {
+    if ( typeof define === 'function' && define.amd ) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        // Node/CommonJS style for Browserify
+        module.exports = factory;
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
     var $Window=$(window),
         windowWidth=$Window.width(),
         windowHeight=$Window.height(),
@@ -527,4 +538,4 @@ $(function(){
         XPop:function(settings){return new XPop(this,settings)}
     });
     window.XPop=XPop;
-});
+}));
